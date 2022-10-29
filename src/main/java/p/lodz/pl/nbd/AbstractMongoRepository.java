09df6b4a04932025ea399cf1bb5f9d22693e87c2
rostigroten <wiki.plonka9501@gmail.com>
2022-10-29 22:52:27 +0200

    codec

---
+++ src/main/java/p/lodz/pl/nbd/AbstractMongoRepository.java
@@ -0,0 +1,48 @@
+package p.lodz.pl.nbd;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.bson.UuidRepresentation;
+import org.bson.codecs.configuration.CodecRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.Conventions;
+import org.bson.codecs.pojo.PojoCodecProvider;
+import p.lodz.pl.nbd.codecs.UniqueIdCodecProvider;
+
+import java.util.List;
+
+public abstract class AbstractMongoRepository implements AutoCloseable{
+
+    private ConnectionString connectionString = new ConnectionString("mongodb://loacalhost:27017");
+    private MongoCredential credential = MongoCredential.createCredential("admin", "admin", "adminpassword".toCharArray());
+
+    private CodecRegistry pojoCodecRegistry =
+            CodecRegistries.fromProviders(PojoCodecProvider.builder()
+                    .automatic(true)
+                    .conventions(List.of(Conventions.ANNOTATION_CONVENTION))
+                    .build());
+
+    private MongoClient mongoClient;
+    private MongoDatabase nbdMongo;
+
+    public void initDbConnection(){
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .credential(credential)
+                .applyConnectionString(connectionString)
+                .uuidRepresentation(UuidRepresentation.STANDARD)
+                .codecRegistry(CodecRegistries.fromRegistries(
+                        CodecRegistries.fromProviders(new UniqueIdCodecProvider()),
+                        MongoClientSettings.getDefaultCodecRegistry(),
+                        pojoCodecRegistry))
+                .build();
+
+        mongoClient = MongoClients.create(settings);
+        nbdMongo = mongoClient.getDatabase("nbdmongo");
+    }
+
+
+}
