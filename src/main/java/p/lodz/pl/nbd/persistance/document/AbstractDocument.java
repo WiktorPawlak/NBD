35747176a4ef1633b4d75db5a1e0903767f32e21
WiktorPawlak <pawlak4112@wp.pl>
2022-10-31 23:41:00 +0100

    fix doubled id

--- src/main/java/p/lodz/pl/nbd/persistance/document/AbstractDocument.java
+++ src/main/java/p/lodz/pl/nbd/persistance/document/AbstractDocument.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.UUID;
 
 import org.bson.codecs.pojo.annotations.BsonCreator;
+import org.bson.codecs.pojo.annotations.BsonId;
 import org.bson.codecs.pojo.annotations.BsonProperty;
 
 import lombok.AccessLevel;
@@ -16,8 +17,8 @@ import lombok.experimental.SuperBuilder;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class AbstractDocument implements Serializable {
-
-    @BsonProperty("id")
+    
+    @BsonId
     private UUID id;
 
     @BsonCreator
